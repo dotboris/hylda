f41d3a6e3c5ee75257155d0dece79c29655b6d41
Boris Bera <bbera@pmctire.com>
2017-05-07 18:57:10 -0400

    Remove old icons filter

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ const postcss = require('gulp-postcss')
 const autoprefixer = require('autoprefixer')
 const stylelint = require('gulp-stylelint')
 const svgSprite = require('gulp-svg-sprite')
-const filter = require('gulp-filter')
 const rename = require('gulp-rename')
 const icons = require('./build/icons.js')
 
@@ -30,12 +29,6 @@ gulp.task('sass', () =>
     .pipe(postcss([autoprefixer()]))
     .pipe(gulp.dest('static/css/'))
 )
-
-const ICONS = ['menu', 'chevron-right', 'chevron-left']
-  .map(i => `${i}.svg`)
-function filterIcons (file) {
-  return ICONS.indexOf(file.relative) !== -1
-}
 
 gulp.task('icons', () =>
   gulp.src('icons.txt')
