00451f910d60cdb380c21707c192ba04ad06630d
Boris Bera <bera.boris@gmail.com>
2017-08-05 22:49:13 -0400

    Build typography with ms()

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,7 +2,9 @@ module.exports = {
   extends: 'stylelint-config-standard',
   rules: {
     'at-rule-no-unknown': [true, {
-      ignoreAtRules: ['mixin', 'content', 'include', 'if', 'else', 'function']
+      ignoreAtRules: [
+        'mixin', 'content', 'include', 'if', 'else', 'function', 'each', 'for'
+      ]
     }]
   }
 }
