25fde797249df3e33ddd4746f5f7fe6f21f4e4ba
Boris Bera <bera.boris@gmail.com>
2017-08-05 20:25:05 -0400

    Extract linting outside of gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,27 +3,12 @@ const sass = require('gulp-sass')
 const postcss = require('gulp-postcss')
 const cssnano = require('cssnano')
 const autoprefixer = require('autoprefixer')
-const stylelint = require('gulp-stylelint')
 const svgSprite = require('gulp-svg-sprite')
 const rename = require('gulp-rename')
 const icons = require('./build/icons')
 const sourcemaps = require('gulp-sourcemaps')
 
 const SASS_FILES = 'sass/**/*.scss'
-
-let watching = false
-
-gulp.task('lint:sass', () =>
-  gulp.src(SASS_FILES)
-    .pipe(stylelint({
-      failOnError: !watching,
-      reporters: [
-        {formatter: 'string', console: true}
-      ]
-    }))
-)
-
-gulp.task('lint', ['lint:sass'])
 
 gulp.task('sass', () =>
   gulp.src(SASS_FILES)
@@ -59,8 +44,7 @@ gulp.task('icons', () =>
 
 gulp.task('default', ['sass', 'icons'])
 
-gulp.task('watch', ['default', 'lint'], () => {
-  watching = true
+gulp.task('watch', ['default'], () => {
   gulp.watch('icons.txt', ['icons'])
-  gulp.watch(SASS_FILES, ['sass', 'lint:sass'])
+  gulp.watch(SASS_FILES, ['sass'])
 })
