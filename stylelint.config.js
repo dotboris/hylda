7871f53d2691d5c22666ae861e74adf6d7297b3e
Boris Bera <bera.boris@gmail.com>
2019-01-09 21:51:02 -0500

    Ignore stylelint rule about descending specifity.
    It's hard to maintain that when using scss and nesting

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: 'stylelint-config-standard',
   rules: {
+    'no-descending-specificity': null,
     'at-rule-no-unknown': [true, {
       ignoreAtRules: [
         'mixin', 'content', 'include', 'if', 'else', 'function', 'each', 'for',
